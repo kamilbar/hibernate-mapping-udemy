ab2f0398f84c2701f9ee76d727b82c81c3a9688d
Kamil Barcik <kamilbar89@gmail.com>
2022-09-11 18:08:23 +0200

    Initial commit

---
+++ src/main/java/com/training/hibernate/JdbcConnectionTest.java
@@ -0,0 +1,22 @@
+package com.training.hibernate;
+
+import com.mysql.cj.jdbc.JdbcConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcConnectionTest {
+
+    public static void main(String[] args) {
+        String user = "hbstudent";
+        String pass = "hbstudent";
+        String Url = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni";
+
+        try(Connection connection = DriverManager.getConnection(Url, user, pass)) {
+            System.out.println("Connection Successful!");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
